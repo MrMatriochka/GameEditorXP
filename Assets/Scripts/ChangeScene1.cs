e178f68a5c550e869c02867c28852407e24480ab
valentinDenat <vdenat31@gmail.com>
2023-02-14 16:59:29 -0500

    ALPHA

---
+++ Assets/Scripts/ChangeScene1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void ChangeSceneString(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
