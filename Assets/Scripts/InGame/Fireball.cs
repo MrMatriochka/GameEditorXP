611beac7b383f1d9d6ac92b59cce6a611d89772d
Andoni Iturioz <ituriozandoni@gmail.com>
2023-04-11 13:58:51 -0400

    save v1

--- Assets/Scripts/InGame/Fireball.cs
+++ Assets/Scripts/InGame/Fireball.cs
@@ -6,6 +6,12 @@ public class Fireball : MonoBehaviour
 {
     public float speed = 1;
     // Update is called once per frame
+
+    private void Start()
+    {
+        if (speed < 0)
+            transform.localScale *= -1;
+    }
     void Update()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
--- Assets/Scripts/Prog/PreviewBoss.cs
+++ Assets/Scripts/Prog/PreviewBoss.cs
@@ -14,7 +14,7 @@ public class PreviewBoss : MonoBehaviour
     public GameObject mouth;
     void FixedUpdate()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left*facing,20);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left*facing,2000);
 
         if (hit.collider != null)
         {
