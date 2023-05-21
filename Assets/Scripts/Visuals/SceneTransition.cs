3cd1e5f785978ee4106cf51fbbbf865ab774b27d
sb810 <sb810@hotmail.com>
2023-05-21 15:41:33 -0400

    Added speed parameter to transition (everything else can be changed in anim), added fireball particle FX, updated fireball sprite template

--- Assets/Scripts/Visuals/SceneTransition.cs
+++ Assets/Scripts/Visuals/SceneTransition.cs
@@ -1,23 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SceneTransition : MonoBehaviour
 {
+    [Header("Transition Settings")]    
+    public Animator anim;    
+    public float speed = 1;
+
+    [Header("Loader Settings")]
     public SceneAsset scene;
-    public Animator anim;
     
     public void Transition()
     {
         anim.SetBool("Started", true);
+        anim.speed = speed;
     }
 
     public void LoadScene()
     {
-        SceneManager.LoadScene(scene.name);
+        if(scene != null)
+            SceneManager.LoadScene(scene.name);
+        else anim.SetBool("Started", false);
     }
 }
